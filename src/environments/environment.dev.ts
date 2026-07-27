d40a127268a5540b8252a705647d66063c00970f
Drakoxw <dragon12xw@gmail.com>
2026-07-26 20:22:58 -0500

    feat: se corrigen el enviroment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:9099/api/v1',
-  API_BASE_URL: 'http://localhost:9099',
+  API_URL: 'https://back-dev.road2theoscars.tech/api/v1',
+  API_BASE_URL: 'https://back-dev.road2theoscars.tech',
 };
 
 // export const environment = {
 //   production: false,
-//   API_URL: 'https://back-dev.road2theoscars.tech/api/v1',
-//   API_BASE_URL: 'https://back-dev.road2theoscars.tech',
+//   API_URL: 'http://localhost:9099/api/v1',
+//   API_BASE_URL: 'http://localhost:9099',
 // };
